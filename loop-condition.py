93d6f4b662da2d0b4de1f7982aab674ab0919c4b
jenonojeno <airrgadq@cgmail.com>
2022-07-05 14:24:57 +0700

    first commit

---
+++ loop-condition.py
@@ -0,0 +1,37 @@
+for i in range(1,11,1):
+    print(1)
+for J in range(5):
+    print(J)
+list1=['hello','hello','hedeyang','yoo','nor','emea']
+for e in list1:
+    print(e)
+Dic ={'name':'tuu','job':'solder', 'age':'25'}
+for key,value in Dic.items():
+    print(key,value)
+i=1
+#while is infinite loop
+while i < 6:
+    print(i)
+    i += 1
+a = False
+if a:
+    print("a is true")
+a = 33
+b = 1
+if b > a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+a = False
+if a:
+    print("a is true")
+else:
+    print("a is false")
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
